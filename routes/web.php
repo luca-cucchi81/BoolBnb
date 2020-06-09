e35c08a65dc8e3f70a882e169dfc2b0fcf33966a
Jasmin-Mediani <molihua1992@gmail.com>
2020-06-09 11:13:08 +0200

    create controllers and routes

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('admin')
+->namespace('Admin')
+->name('admin.')
+->middleware('auth')
+->group(function(){
+    Route::resource('apartments', 'ApartmentController');
+    Route::resource('users', 'UserController');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
