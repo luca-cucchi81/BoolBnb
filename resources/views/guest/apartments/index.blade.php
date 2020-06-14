58bd96dea3bee24f4dad4cc0fd1312ee17e20bc6
Jasmin-Mediani <molihua1992@gmail.com>
2020-06-14 17:00:21 +0200

    hidden inputs for lat and long

--- resources/views/guest/apartments/index.blade.php
+++ resources/views/guest/apartments/index.blade.php
@@ -18,6 +18,8 @@
                 @method('GET')
                 <div class="form-group">
                     <input type="search" id="address" name="address" class="form-control" placeholder="Dove vuoi andare?">
+                    <input type="hidden" id="lat" name="lat" class="form-control">
+                    <input type="hidden" id="lng" name="lng" class="form-control">
                     <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
                     <script>
                         (function() {
@@ -28,7 +30,12 @@
                             });
                             var $address = document.querySelector('#address-value')
                             placesAutocomplete.on('change', function(e) {
-                                $address.textContent = e.suggestion.value
+                                $('#address').val(e.suggestion.value);  //ora è scritto bene
+                                $('#lat').val(e.suggestion.latlng.lat);
+                                $('#lng').val(e.suggestion.latlng.lng);
+                                
+                                console.log("latitudine: ", $('#lat').val());
+                                console.log("longitudine: ", $('#lng').val());
                             });
                             placesAutocomplete.on('clear', function() {
                                 $address.textContent = 'none';
